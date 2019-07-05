1d237b65b297a46f3baa16c5f3512d11dc271fd2
Tillmann Heidsieck <theidsieck@leenox.de>
2019-07-05 20:40:19 +0200

    run: speed up re-own
    Signed-off-by: Tillmann Heidsieck <theidsieck@leenox.de>

--- run.sh
+++ run.sh
@@ -15,7 +15,8 @@ echo -e "RUN_AS='$RUN_AS'\nRUN_AS_GROUP='$RUN_AS_GROUP'" > /run_as
 sed -e "s/www-data/$RUN_AS/g" -i /etc/php/7.2/fpm/pool.d/www.conf
 sed -e "s/user \+nginx;/user $RUN_AS;/" -i /etc/nginx/nginx.conf
 
-chown -R $RUN_AS.$RUN_AS /srv
+#chown -R $RUN_AS.$RUN_AS /srv
+find /srv \! -user $RUN_AS -print | xargs chown $RUN_AS.$RUN_AS_GROUP
 
 if [[ -f /srv/config/config.php ]]; then
 	/usr/bin/occ upgrade
